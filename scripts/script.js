c2c83a63e81f0f6a71e968c4e939f2574798f20e
eric rufino <aluno103.23187232eric@gmail.com>
2024-04-26 21:55:31 -0300

    interface crud v1.3

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
-document.getElementById("userRegistration").addEventListener("click", modalOpen);
+document
+  .getElementById("userRegistration")
+  .addEventListener("click", modalOpen);
 document.getElementById("modalClose").addEventListener("click", modalClose);
 document.getElementById("saveValues").addEventListener("click", saveValues);
+document.getElementById("cancelButton").addEventListener("click", modalClose);
 
 function modalOpen() {
   document.querySelector("h2").innerText = "Novo Usuário";
@@ -14,14 +17,12 @@ function modalClose() {
   document.getElementById("email").value = "";
   document.getElementById("cel").value = "";
   document.getElementById("city").value = "";
-
 }
 
 const celInput = document.getElementById("cel");
 celInput.addEventListener("input", function (event) {
-  let value = event.target.value.replace(/\D/g, ""); // Remove todos os caracteres que não são dígitos
+  let value = event.target.value.replace(/\D/g, "");
 
-  // Formatação do número de telefone (00) 00000-0000
   let formattedValue = "";
   if (value.length > 0) {
     formattedValue += "(" + value.substring(0, 2);
@@ -32,8 +33,6 @@ celInput.addEventListener("input", function (event) {
   if (value.length > 7) {
     formattedValue += "-" + value.substring(7, 11);
   }
-
-  // Atualiza o valor do input com a formatação aplicada
   event.target.value = formattedValue;
 });
 
@@ -79,16 +78,18 @@ function saveValues() {
     document.getElementById("city").value = "";
 
     modalClose();
+    insertTable(); // Chama a função para atualizar a tabela
+
   } else {
     alert("Preencha todos os campos!!!");
   }
-  window.location.reload();
 }
 
 function insertTable() {
-  const tableData = document.getElementById("tbody");
+  let listData = [];
+  let tableData = document.getElementById("tbody");
   if (localStorage.getItem("users")) {
-    const listData = JSON.parse(localStorage.getItem("users"));
+    listData = JSON.parse(localStorage.getItem("users"));
     let template = "";
 
     listData.forEach((user) => {
@@ -98,20 +99,23 @@ function insertTable() {
             <td> ${user.cel}</td>
             <td> ${user.city}</td>
             <td>
-                <button type="button" class="button green" onclick="editUser(${user.idUser})">Editar</button>
-                <button type="button" class="button red">Excluir</button>
+                <button type="button" class="button green" onclick="updateUser(${user.idUser})">Editar</button>
+                <button type="button" class="button red" onclick="deleteUser(${user.idUser})">Excluir</button>
             </td>
         </tr>`;
     });
     tableData.innerHTML = template;
-  } else {
-    tableData.innerHTML = `<tr><td colspan="5"> Nenhum usuario cadastradao!</td></tr>`;
+  }
+
+  if (listData.length == 0) {
+    tableData.innerHTML = `<tr><td class="empty-message" colspan="5"> Nenhum usuário cadastrado!</td></tr>`;
   }
 }
 
-function editUser(idUser) {
-
-  document.getElementById('saveValues').removeEventListener('click', saveValues);
+function updateUser(idUser) {
+  document
+    .getElementById("saveValues")
+    .removeEventListener("click", saveValues);
 
   modalOpen();
   const textTitleUpdateUser = document.querySelector("h2");
@@ -126,18 +130,52 @@ function editUser(idUser) {
   document.getElementById("cel").value = `${userData.cel}`;
   document.getElementById("city").value = `${userData.city}`;
 
-  document.getElementById('saveValues').addEventListener('click', uptadeUserInfo);
+  document
+    .getElementById("saveValues")
+    .addEventListener("click", () => updateUserInfo(idUser));
 }
 
-function uptadeUserInfo(){
-  const newName = document.getElementById('name').value;
-  const newEmail = document.getElementById('email').value;
-  const newCel = document.getElementById('cel').value;
-  const newCity = document.getElementById('city').value;
+function updateUserInfo(idUser) {
+  const newName = document.getElementById("name").value;
+  const newEmail = document.getElementById("email").value;
+  const newCel = document.getElementById("cel").value;
+  const newCity = document.getElementById("city").value;
 
-  alert('Atualizado!')
+  if(newName && newEmail && newCel && newCity){
+  const userList = JSON.parse(localStorage.getItem("users")) || [];
+  const userIndexFind = userList.findIndex((user) => user.idUser == idUser);
 
+ 
+
+  if (userIndexFind !== -1) {
+    userList[userIndexFind].name = newName;
+    userList[userIndexFind].cel = newCel;
+    userList[userIndexFind].email = newEmail;
+    userList[userIndexFind].city = newCity;
+
+    localStorage.setItem("users", JSON.stringify(userList));
+
+    modalClose();
+  }
+}else{
+  alert("Prencha todos os dados para atualizar!")
+  return;
+}
+  window.location.reload();
 }
 
 window.addEventListener("DOMContentLoaded", insertTable);
+
+function deleteUser(idUser) {
+  const getUserData = JSON.parse(localStorage.getItem("users"));
+
+  const findUser = getUserData.findIndex((user) => user.idUser == idUser);
+
+  if (findUser !== -1) {
+    getUserData.splice(findUser, 1);
+    localStorage.setItem("users", JSON.stringify(getUserData));
+    insertTable();
+
+  }
+}
 
